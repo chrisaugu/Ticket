f1aae1532e0085ab81a9d303550b3941660bd1a3
Christian Augustyn <30349879+chrisaugu@users.noreply.github.com>
2022-12-23 19:28:51 +1000

    remove unnecessary files

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './reduxStore';
 
-import reportWebVitals from './reportWebVitals';
-
 import "biings-ds/build/bds.css";
 import './index.css';
 
@@ -21,8 +19,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);
